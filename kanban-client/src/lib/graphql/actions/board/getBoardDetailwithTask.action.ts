03a63131090c1771ab83e32e3403e71ba4e09056
haolk003 <nguyenquochaolop91@gmail.com>
2025-05-13 21:27:59 +0700

    Add new UI components, GraphQL query, health module, and DB migrations
    Introduce `Calendar`, `RadioGroup`, `Textarea`, and `Tooltip` UI components. Add `GET_BOARD_DETAIL_WITH_TASK` GraphQL query for fetching board details with tasks. Implement `health` module in the server. Apply database migrations to update `List` and `FileAttachment` models.

---
+++ kanban-client/src/lib/graphql/actions/board/getBoardDetailwithTask.action.ts
@@ -0,0 +1,55 @@
+import { gql } from "@apollo/client";
+
+
+export const GET_BOARD_DETAIL_WITH_TASK = gql`
+  query getBoardDetailWithTask($id: String!) {
+    getBoardDetailById(id: $id) {
+      id
+      title
+      description
+      projectKey
+      lists{
+        id
+        name
+        tasks{
+          taskId
+          counts{
+            subTask
+            likes
+            discussion
+          }
+          id
+          title
+          description
+          createdAt
+          labels{
+            id
+            name
+          }
+          dueDate
+          assignedTo{
+            user{
+              id
+              name
+              avatar{
+                url
+                publicId
+              }
+
+            }
+          }
+
+        }
+      }
+      member{
+        user{
+          id
+          name
+          avatar{
+            url
+          }
+        }
+      }
+    }
+  }
+`;
